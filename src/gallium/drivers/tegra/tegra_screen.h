49dbdedbd6c248e5b4b2d859bf14cf866905ef22
Thierry Reding <thierry.reding@avionic-design.de>
2013-03-26 14:14:49 +0100

    tegra: initial gallium driver for NVIDIA Tegra
    Add a stub Gallium driver for NVIDIA Tegra SoCs to serve as a basis for
    future development.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Nicolas Chauvet <kwizart@gmail.com>
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

---
+++ src/gallium/drivers/tegra/tegra_screen.h
@@ -0,0 +1,24 @@
+#ifndef TEGRA_SCREEN_H
+#define TEGRA_SCREEN_H
+
+#include "pipe/p_screen.h"
+#include "util/slab.h"
+
+#include <libdrm/tegra.h>
+
+struct tegra_screen {
+   struct pipe_screen base;
+
+   struct drm_tegra *drm;
+};
+
+static inline struct tegra_screen *
+tegra_screen(struct pipe_screen *screen)
+{
+   return (struct tegra_screen *)screen;
+}
+
+struct pipe_screen *
+tegra_screen_create(struct drm_tegra *drm);
+
+#endif
