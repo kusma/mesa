7f66ce149c0ccd09a7f6f7fcc5185edf93776c43
Erik Faye-Lund <kusmabite@gmail.com>
2017-05-10 23:07:36 +0000

    tegra: prepare for draw

--- src/gallium/drivers/tegra/tegra_common.h
+++ src/gallium/drivers/tegra/tegra_common.h
@@ -3,4 +3,11 @@
 
 #define unimplemented() printf("TODO: %s()\n", __func__)
 
+#define TGR3D_VAL(reg_name, field_name, value) \
+   (((value) << TGR3D_ ## reg_name ## _ ## field_name ## __SHIFT) & \
+           TGR3D_ ## reg_name ## _ ## field_name ## __MASK)
+
+#define TGR3D_BOOL(reg_name, field_name, boolean) \
+   ((boolean) ? TGR3D_ ## reg_name ## _ ## field_name : 0)
+
 #endif
