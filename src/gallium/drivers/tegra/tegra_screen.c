01ebd7d530b4155c046f6e46ef0b280e1df7d176
Erik Faye-Lund <kusmabite@gmail.com>
2017-05-30 23:30:10 +0000

    tegra: use user-buffers for constants

--- src/gallium/drivers/tegra/tegra_screen.c
+++ src/gallium/drivers/tegra/tegra_screen.c
@@ -160,8 +160,10 @@ tegra_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return 0; /* no idea, need to test */
 
    case PIPE_CAP_USER_VERTEX_BUFFERS:
-   case PIPE_CAP_USER_CONSTANT_BUFFERS:
       return 0; /* probably possible, but nasty for kernel */
+
+   case PIPE_CAP_USER_CONSTANT_BUFFERS:
+      return 1;
 
    case PIPE_CAP_VERTEX_BUFFER_OFFSET_4BYTE_ALIGNED_ONLY:
    case PIPE_CAP_VERTEX_BUFFER_STRIDE_4BYTE_ALIGNED_ONLY:
