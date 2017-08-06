c57c72013c03bd42bc60ab4034174613e0980b79
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-06 22:01:56 +0200

    HACK: tegra: implement fences

--- src/gallium/drivers/tegra/tegra_fence.h
+++ src/gallium/drivers/tegra/tegra_fence.h
@@ -19,4 +19,19 @@ tegra_fence(struct pipe_fence_handle *fence)
    return (struct tegra_fence *)fence;
 }
 
+static inline struct tegra_fence *
+tegra_fence_create(struct host1x_fence *fence)
+{
+   struct tegra_fence *ret;
+
+   ret = CALLOC_STRUCT(tegra_fence);
+   if (!ret)
+      return NULL;
+
+   pipe_reference_init(&ret->reference, 1);
+   ret->fence = fence;
+
+   return ret;
+}
+
 #endif
--- src/gallium/drivers/tegra/tegra_screen.c
+++ src/gallium/drivers/tegra/tegra_screen.c
@@ -523,6 +523,31 @@ tegra_screen_is_format_supported(struct pipe_screen *pscreen,
    return true;
 }
 
+static void
+tegra_screen_fence_reference(struct pipe_screen *pscreen,
+                             struct pipe_fence_handle **ptr,
+                             struct pipe_fence_handle *fence)
+{
+//    assert(ptr && *ptr);
+//    assert(fence);
+//
+//    if (pipe_reference(&tegra_fence(*ptr)->reference, &tegra_fence(fence)->reference)) {
+//       FREE(tegra_fence(*ptr)->fence);
+//       FREE(*ptr);
+//    }
+//
+//    *ptr = fence;
+}
+
+static boolean
+tegra_screen_fence_finish(struct pipe_screen *screen,
+                          struct pipe_context *ctx,
+                          struct pipe_fence_handle *fence,
+                          uint64_t timeout)
+{
+   return 0;
+}
+
 struct pipe_screen *
 tegra_screen_create(struct drm_tegra *drm)
 {
@@ -540,6 +565,10 @@ tegra_screen_create(struct drm_tegra *drm)
    screen->base.get_shader_param = tegra_screen_get_shader_param;
    screen->base.context_create = tegra_screen_context_create;
    screen->base.is_format_supported = tegra_screen_is_format_supported;
+
+   /* fence functions */
+   screen->base.fence_reference = tegra_screen_fence_reference;
+   screen->base.fence_finish = tegra_screen_fence_finish;
 
    tegra_screen_resource_init(&screen->base);
 
