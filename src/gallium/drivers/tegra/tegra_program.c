0a7f17d5f42e0c2e5225c42c024461e1ba4fb847
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-14 22:33:13 +0200

    HACK: create a simple VPE IR

--- src/gallium/drivers/tegra/tegra_program.c
+++ src/gallium/drivers/tegra/tegra_program.c
@@ -8,7 +8,101 @@
 #include "tegra_context.h"
 #include "tegra_screen.h"
 #include "tegra_program.h"
+#include "vpe_ir.h"
 
+static struct vpe_src_operand
+src_undef()
+{
+   struct vpe_src_operand ret = {};
+   ret.file = VPE_SRC_FILE_UNDEF;
+   ret.index = 0;
+   ret.swizzle[0] = VPE_SWZ_X;
+   ret.swizzle[1] = VPE_SWZ_Y;
+   ret.swizzle[2] = VPE_SWZ_Z;
+   ret.swizzle[3] = VPE_SWZ_W;
+   return ret;
+}
+
+static struct vpe_src_operand
+attrib(int index, const enum vpe_swz swizzle[4])
+{
+   struct vpe_src_operand ret = {};
+   ret.file = VPE_SRC_FILE_ATTRIB;
+   ret.index = index;
+   memcpy(ret.swizzle, swizzle, sizeof(ret.swizzle));
+   return ret;
+}
+
+static struct vpe_src_operand
+uniform(int index, const enum vpe_swz swizzle[4])
+{
+   struct vpe_src_operand ret = {};
+   ret.file = VPE_SRC_FILE_UNIFORM;
+   ret.index = index;
+   memcpy(ret.swizzle, swizzle, sizeof(ret.swizzle));
+   return ret;
+}
+
+
+static struct vpe_dst_operand
+output(int index, unsigned int write_mask)
+{
+   struct vpe_dst_operand ret = {};
+   ret.file = VPE_DST_FILE_OUTPUT;
+   ret.index = index;
+   ret.write_mask = write_mask;
+   return ret;
+}
+
+static struct vpe_vec_instr
+emit_vec_unop(enum vpe_vec_op op, struct vpe_dst_operand dst,
+              struct vpe_src_operand src)
+{
+   struct vpe_vec_instr ret = {};
+   ret.op = op;
+   ret.dst = dst;
+   ret.src[0] = src;
+   ret.src[1] = src; // src_undef();
+   ret.src[2] = src; // src_undef();
+   return ret;
+}
+
+static struct vpe_vec_instr
+emit_vMOV(struct vpe_dst_operand dst, struct vpe_src_operand src)
+{
+   return emit_vec_unop(VPE_VEC_OP_MOV, dst, src);
+}
+
+static struct vpe_vec_instr
+emit_vADD(struct vpe_dst_operand dst, struct vpe_src_operand src0,
+          struct vpe_src_operand src2)
+{
+   struct vpe_vec_instr ret = {};
+   ret.op = VPE_VEC_OP_ADD;
+   ret.dst = dst;
+   ret.src[0] = src0;
+   ret.src[1] = src_undef(); // add is "strange" in that it takes src0 and src2
+   ret.src[2] = src2;
+   return ret;
+}
+
+static struct vpe_scalar_instr
+emit_sNOP()
+{
+   struct vpe_scalar_instr ret = {};
+   ret.op = VPE_SCALAR_OP_NOP;
+   /* TODO: fill in more */
+   return ret;
+}
+
+static struct vpe_instr
+emit_packed(struct vpe_vec_instr vec, struct vpe_scalar_instr scalar)
+{
+   struct vpe_instr ret = {};
+   ret.vec = vec;
+   ret.scalar = scalar;
+   return ret;
+}
 
 static void *
 tegra_create_vs_state(struct pipe_context *pcontext,
---
+++ src/gallium/drivers/tegra/vpe_ir.c
@@ -0,0 +1,102 @@
+#include <assert.h>
+
+#include "util/macros.h"
+
+#include "vpe_ir.h"
+#include "vpe_vliw.h"
+
+void
+tegra_vpe_pack(uint32_t *dst, struct vpe_instr instr, bool end_of_program)
+{
+   vpe_instr128 tmp = {
+      .predicate_lt = 1,
+      .predicate_eq = 1,
+      .predicate_gt = 1,
+
+      .predicate_swizzle_x = SWIZZLE_X,
+      .predicate_swizzle_y = SWIZZLE_Y,
+      .predicate_swizzle_z = SWIZZLE_Z,
+      .predicate_swizzle_w = SWIZZLE_W,
+   };
+
+   int attr_fetch = -1, uniform_fetch = -1;
+   for (int i = 0; i < 3; ++i) {
+      switch (instr.vec.src[i].file) {
+      case VPE_SRC_FILE_ATTRIB:
+         assert(attr_fetch < 0 ||
+                attr_fetch == instr.vec.src[i].index);
+         attr_fetch = instr.vec.src[i].index;
+         instr.vec.src[i].index = 0;
+         break;
+
+      case VPE_SRC_FILE_UNIFORM:
+         assert(uniform_fetch < 0 ||
+                uniform_fetch == instr.vec.src[i].index);
+         uniform_fetch = instr.vec.src[i].index;
+         instr.vec.src[i].index = 0;
+         break;
+
+      default: /* nothing */
+         break;
+      }
+   }
+
+   tmp.attribute_fetch_index = attr_fetch >= 0 ? attr_fetch : 0;
+   tmp.uniform_fetch_index = uniform_fetch >= 0 ? uniform_fetch : 0;
+
+   tmp.scalar_rD_index = 63; // disable write
+
+   tmp.vector_opcode = instr.vec.op;
+
+   switch (instr.vec.dst.file) {
+   case VPE_DST_FILE_TEMP:
+      tmp.vector_rD_index = instr.vec.dst.index;
+      break;
+
+   case VPE_DST_FILE_OUTPUT:
+      tmp.vector_rD_index = 63; // disable register-write
+      tmp.export_vector_write_enable = 1;
+      tmp.export_write_index = instr.vec.dst.index;
+      break;
+
+   case VPE_DST_FILE_UNDEF:
+      assert(0);  // TODO: consult NOP
+      break;
+
+   default:
+      unreachable("illegal enum vpe_dst_file value");
+   }
+
+   tmp.vector_op_write_x_enable = (instr.vec.dst.write_mask >> 0) & 1;
+   tmp.vector_op_write_y_enable = (instr.vec.dst.write_mask >> 1) & 1;
+   tmp.vector_op_write_z_enable = (instr.vec.dst.write_mask >> 2) & 1;
+   tmp.vector_op_write_w_enable = (instr.vec.dst.write_mask >> 3) & 1;
+
+   tmp.rA_type = instr.vec.src[0].file;
+   tmp.rA_index = instr.vec.src[0].index;
+   tmp.rA_swizzle_x = instr.vec.src[0].swizzle[0];
+   tmp.rA_swizzle_y = instr.vec.src[0].swizzle[1];
+   tmp.rA_swizzle_z = instr.vec.src[0].swizzle[2];
+   tmp.rA_swizzle_w = instr.vec.src[0].swizzle[3];
+
+   tmp.rB_type = instr.vec.src[1].file;
+   tmp.rB_index = instr.vec.src[1].index;
+   tmp.rB_swizzle_x = instr.vec.src[1].swizzle[0];
+   tmp.rB_swizzle_y = instr.vec.src[1].swizzle[1];
+   tmp.rB_swizzle_z = instr.vec.src[1].swizzle[2];
+   tmp.rB_swizzle_w = instr.vec.src[1].swizzle[3];
+
+   tmp.rC_type = instr.vec.src[2].file;
+   tmp.rC_index = instr.vec.src[2].index;
+   tmp.rC_swizzle_x = instr.vec.src[2].swizzle[0];
+   tmp.rC_swizzle_y = instr.vec.src[2].swizzle[1];
+   tmp.rC_swizzle_z = instr.vec.src[2].swizzle[2];
+   tmp.rC_swizzle_w = instr.vec.src[2].swizzle[3];
+
+   tmp.end_of_program = end_of_program;
+
+   dst[0] = tmp.part3;
+   dst[1] = tmp.part2;
+   dst[2] = tmp.part1;
+   dst[3] = tmp.part0;
+}
