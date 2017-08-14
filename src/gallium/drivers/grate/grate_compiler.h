99fc9ca92dba4b8f79f19b06e22007b3f354bec8
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-14 22:33:13 +0200

    HACK: grate: transpile TGSI to VPE

---
+++ src/gallium/drivers/grate/grate_compiler.h
@@ -0,0 +1,18 @@
+#ifndef GRATE_COMPILER_H
+#define GRATE_COMPILER_H
+
+#include "util/list.h"
+
+#include <stdint.h>
+
+struct tgsi_parse_context;
+
+struct tegra_vpe_shader {
+   struct list_head instructions;
+   uint16_t output_mask;
+};
+
+void
+tegra_tgsi_to_vpe(struct tegra_vpe_shader *vpe, struct tgsi_parse_context *tgsi);
+
+#endif
