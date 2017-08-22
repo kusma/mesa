370b2033eade0b6f3b70696241dcad64ae7dfcb2
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-22 15:31:19 +0200

    HACK: tegra: apply index-bias
    Haven't verified that this is the correct usage, but I *think* so.

--- src/gallium/drivers/tegra/tegra_draw.c
+++ src/gallium/drivers/tegra/tegra_draw.c
@@ -93,7 +93,7 @@ tegra_draw_vbo(struct pipe_context *pcontext,
 
    assert(info->count < (1 << 11));
    value  = TGR3D_VAL(DRAW_PRIMITIVES, INDEX_COUNT, info->count);
-   value |= TGR3D_VAL(DRAW_PRIMITIVES, OFFSET, 0); /* TODO: derive from info */
+   value |= TGR3D_VAL(DRAW_PRIMITIVES, OFFSET, info->index_bias);
    tegra_stream_push(stream, host1x_opcode_incr(TGR3D_DRAW_PRIMITIVES, 1));
    tegra_stream_push(stream, value);
 
