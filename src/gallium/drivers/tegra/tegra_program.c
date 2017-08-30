65bd1aebbe8b664c3fadf35b6b3b56eb740f2e54
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-30 20:20:10 +0200

    tegra: add support for dumping TGSI

--- src/gallium/drivers/tegra/tegra_program.c
+++ src/gallium/drivers/tegra/tegra_program.c
@@ -20,6 +20,12 @@ tegra_create_vs_state(struct pipe_context *pcontext,
 
    so->base = *template;
 
+   if (tegra_debug & TEGRA_DEBUG_TGSI) {
+      fprintf(stderr, "DEBUG: TGSI:\n");
+      tgsi_dump(template->tokens, 0);
+      fprintf(stderr, "\n");
+   }
+
    /* TODO: generate code! */
 
    return so;
@@ -46,6 +52,12 @@ tegra_create_fs_state(struct pipe_context *pcontext,
       return NULL;
 
    so->base = *template;
+
+   if (tegra_debug & TEGRA_DEBUG_TGSI) {
+      fprintf(stderr, "DEBUG: TGSI:\n");
+      tgsi_dump(template->tokens, 0);
+      fprintf(stderr, "\n");
+   }
 
    /* TODO: generate code! */
 
