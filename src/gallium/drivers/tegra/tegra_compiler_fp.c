05a566177025565a99fb660ae563eb8efc799f04
Erik Faye-Lund <kusmabite@gmail.com>
2017-12-13 22:57:51 +0100

    hacky nir yo

--- src/gallium/drivers/tegra/tegra_compiler_fp.c
+++ src/gallium/drivers/tegra/tegra_compiler_fp.c
@@ -1,9 +1,13 @@
 #include "tegra_compiler.h"
+#include "tegra_common.h"
 #include "fp_ir.h"
 
 #include "tgsi/tgsi_parse.h"
 
 #include "util/u_memory.h"
+
+#include "nir/tgsi_to_nir.h"
+
 
 static struct fp_alu_src_operand
 fp_alu_src_row(int index)
@@ -200,6 +204,178 @@ emit_tgsi_declaration(struct tegra_fp_shader *fp, const struct tgsi_full_declara
    }
 }
 
+static void
+emit_alu(struct tegra_fp_shader *fp, nir_alu_instr *alu)
+{
+   const nir_op_info *info = &nir_op_infos[alu->op];
+
+   switch (alu->op) {
+/*
+   case nir_op_vec2:
+   case nir_op_vec3:
+   case nir_op_vec4:
+      for (int i = 0; i < info->num_inputs; i++) {
+         nir_alu_src *src = &alu->src[i];
+         printf("lol %d: %d\n", i, src->src.is_ssa);
+      }
+      break;
+*/
+
+   default:
+      printf("emit_alu: not implemented (%s)\n", info->name);
+      break;
+   }
+}
+
+static void
+emit_intrinsic(struct tegra_fp_shader *fp, nir_intrinsic_instr *intr)
+{
+   const nir_intrinsic_info *info = &nir_intrinsic_infos[intr->intrinsic];
+
+   switch (intr->intrinsic) {
+   case nir_intrinsic_load_input:
+      printf("emit_intrinsic: not implemented (load_input)\n");
+      break;
+
+   case nir_intrinsic_store_output:
+      printf("emit_intrinsic: not implemented (store_output)\n");
+      break;
+
+   default:
+      printf("emit_intrinsic: not implemented (%s)\n", info->name);
+   }
+}
+
+static void
+emit_load_const(struct tegra_fp_shader *fp, nir_load_const_instr *instr)
+{
+   printf("emit_load_const: not implemented\n");
+   assert(instr->def.num_components == 1);
+}
+
+static void
+emit_block(struct tegra_fp_shader *fp, struct nir_block *block)
+{
+   nir_foreach_instr(instr, block) {
+      switch (instr->type) {
+      case nir_instr_type_alu:
+         emit_alu(fp, nir_instr_as_alu(instr));
+         break;
+      case nir_instr_type_intrinsic:
+         emit_intrinsic(fp, nir_instr_as_intrinsic(instr));
+         break;
+      case nir_instr_type_load_const:
+         emit_load_const(fp, nir_instr_as_load_const(instr));
+         break;
+      case nir_instr_type_ssa_undef:
+         unreachable("nir_instr_type_ssa_undef not supported");
+         break;
+      case nir_instr_type_tex:
+         unreachable("nir_instr_type_tex not supported");
+         break;
+      case nir_instr_type_phi:
+         unreachable("nir_instr_type_phi not supported");
+         break;
+      case nir_instr_type_jump:
+         unreachable("nir_instr_type_jump not supported");
+         break;
+      case nir_instr_type_call:
+      case nir_instr_type_parallel_copy:
+         unreachable("nir_instr_type_call / nir_instr_type_parallel_copy not supported");
+         break;
+      }
+   }
+}
+
+static void
+emit_cf_list(struct tegra_fp_shader *fp, struct exec_list *list)
+{
+   foreach_list_typed(nir_cf_node, node, node, list) {
+      switch (node->type) {
+      case nir_cf_node_block:
+         emit_block(fp, nir_cf_node_as_block(node));
+         break;
+
+      case nir_cf_node_if:
+         unreachable("nir_cf_node_if not supported");
+         break;
+
+      case nir_cf_node_loop:
+         unreachable("nir_cf_node_loop not supported");
+         break;
+
+      case nir_cf_node_function:
+         unreachable("nir_cf_node_function not supported");
+         break;
+      }
+   }
+}
+
+static void
+optimize_nir(struct nir_shader *s)
+{
+   bool progress;
+   do {
+      progress = false;
+      NIR_PASS_V(s, nir_lower_vars_to_ssa);
+      NIR_PASS(progress, s, nir_lower_alu_to_scalar);
+      NIR_PASS(progress, s, nir_lower_phis_to_scalar);
+      NIR_PASS(progress, s, nir_copy_prop);
+      NIR_PASS(progress, s, nir_opt_remove_phis);
+      NIR_PASS(progress, s, nir_opt_dce);
+      NIR_PASS(progress, s, nir_opt_dead_cf);
+      NIR_PASS(progress, s, nir_opt_cse);
+      NIR_PASS(progress, s, nir_opt_peephole_select, 8);
+      NIR_PASS(progress, s, nir_opt_algebraic);
+      NIR_PASS(progress, s, nir_opt_constant_folding);
+      NIR_PASS(progress, s, nir_opt_undef);
+      NIR_PASS(progress, s, nir_opt_loop_unroll,
+         nir_var_shader_in |
+         nir_var_shader_out |
+         nir_var_local);
+   } while (progress);
+}
+
+static void
+tegra_nir_to_fp(struct tegra_fp_shader *fp, nir_shader *s)
+{
+   NIR_PASS_V(s, nir_opt_global_to_local);
+   NIR_PASS_V(s, nir_lower_regs_to_ssa);
+   NIR_PASS_V(s, nir_lower_load_const_to_scalar);
+
+   optimize_nir(s);
+
+   NIR_PASS_V(s, nir_lower_locals_to_regs);
+   NIR_PASS_V(s, nir_convert_from_ssa, true);
+   NIR_PASS_V(s, nir_remove_dead_variables, nir_var_local);
+   NIR_PASS_V(s, nir_move_vec_src_uses_to_dest);
+   NIR_PASS_V(s, nir_lower_vec_to_movs);
+
+   nir_print_shader(s, stdout);
+
+   nir_function_impl *entry = nir_shader_get_entrypoint(s);
+   emit_cf_list(fp, &entry->body);
+}
+
+static const nir_shader_compiler_options options = {
+   .lower_fpow = true,
+   .lower_fsat = true,
+   .lower_scmp = true,
+   .lower_flrp32 = true,
+   .lower_flrp64 = true,
+   .lower_ffract = true,
+   .lower_fmod32 = true,
+   .lower_fmod64 = true,
+   .lower_fdiv = true,
+   .fuse_ffma = true,
+};
+
+static struct nir_shader *
+fp_tgsi_to_nir(const struct tgsi_token *tokens)
+{
+   return tgsi_to_nir(tokens, &options);
+}
+
 void
 tegra_tgsi_to_fp(struct tegra_fp_shader *fp, const struct tgsi_token *tokens)
 {
@@ -211,6 +387,10 @@ tegra_tgsi_to_fp(struct tegra_fp_shader *fp, const struct tgsi_token *tokens)
    fp->info.color_input = -1;
    fp->info.max_tram_row = 1;
 
+#if 1
+   struct nir_shader *s = fp_tgsi_to_nir(tokens);
+   tegra_nir_to_fp(fp, s);
+#else
    struct tgsi_parse_context parser;
    int ret = tgsi_parse_init(&parser, tokens);
    assert(ret == TGSI_PARSE_OK);
@@ -228,7 +408,7 @@ tegra_tgsi_to_fp(struct tegra_fp_shader *fp, const struct tgsi_token *tokens)
          break;
       }
    }
-
+#endif
    /*
     * HACK: insert barycentric interpolation setup
     * This will overwrite instructions in some cases, need proper scheduler
